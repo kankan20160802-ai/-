4dee261a7119d0ded06b870e4184487919e8ec8c
kankan20160802-ai <kankan20160802@gmail.com>
2026-08-22 16:48:09 +0900

    Update Vite React plugin import

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true, // すべてのホストを許可してブロックを解除します
+    allowedHosts: true, // ブロックを解除します
   }
 })
